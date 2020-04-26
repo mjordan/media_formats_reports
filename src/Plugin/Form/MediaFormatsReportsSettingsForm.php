1f0a5b1b31c2dd6c870cfe5a3c5ba250a4f36e98
Mark Jordan <mjordan@sfu.ca>
2020-04-26 17:21:29 +0000

    Added admin option to cache data.

--- src/Plugin/Form/MediaFormatsReportsSettingsForm.php
+++ src/Plugin/Form/MediaFormatsReportsSettingsForm.php
@@ -38,7 +38,12 @@ class MediaFormatsReportsSettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('media_formats_reports_media_use_terms'),
       '#description' => $this->t('A comma-separated list of the IDs of the terms from the Islandora Media Use vocabulary to include in the MIME Type report.'),
     ];
-
+    $form['media_formats_reports_cache_report_data'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Cache report data.'),
+      '#default_value' => $config->get('media_formats_reports_cache_report_data'),
+      '#description' => $this->t('Generating data to populate charts can take a long time. Check this if you want to cache the data, or if you pregenerate your data using Drush.'),
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -48,6 +53,7 @@ class MediaFormatsReportsSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->configFactory->getEditable('media_formats_reports.settings')
       ->set('media_formats_reports_media_use_terms', $form_state->getValue('media_formats_reports_media_use_terms'))
+      ->set('media_formats_reports_cache_report_data', $form_state->getValue('media_formats_reports_cache_report_data'))
       ->save();
 
     parent::submitForm($form, $form_state);
